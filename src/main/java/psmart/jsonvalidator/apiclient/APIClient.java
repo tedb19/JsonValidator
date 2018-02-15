5d80667e4b67b95d23edf5989c2e6f6b99d33aed
Teddy Odhiambo <tedb19@gmail.com>
2018-02-15 14:46:43 +0000

    first commit

---
+++ src/main/java/psmart/jsonvalidator/apiclient/APIClient.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psmart.jsonvalidator.apiclient;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import psmart.jsonvalidator.mapper.SHR;
+
+/**
+ *
+ * @author tedb19
+ */
+public class APIClient {
+
+    public static String getSHRStr(String SHRURL, String cardSerial) {
+        String SHRJsonStr = "";
+        try {
+            //cardSerial will be passed to the GET endpoint
+            URL url = new URL(SHRURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            System.out.println("Output from Server .... \n");
+            String output = "";
+            while ((output = br.readLine()) != null) {
+                //System.out.println(SHRJsonStr);
+                SHRJsonStr += output;
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            System.out.println("MalformedURLException: " + e.getMessage());
+
+        } catch (IOException e) {
+
+            System.out.println("IOException: " + e.getMessage());
+
+        }
+        return SHRJsonStr;
+    }
+    
+    public static String postSHR(String SHRURL, String SHRStr) {
+        String response = "";
+        try {
+            URL url = new URL(SHRURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = conn.getOutputStream();
+            os.write(SHRStr.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                response += output;
+            }
+
+            conn.disconnect();
+            
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return response;
+    }
+
+}
